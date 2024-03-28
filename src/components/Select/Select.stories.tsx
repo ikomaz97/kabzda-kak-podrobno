190932a2ced8344746ec3d4d0f1622701bbebd5b
ikomaz97 <ikomaz97@gmail.com>
2024-03-28 14:59:17 +0400

    04

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -9,4 +9,30 @@ export default  {
 
 
 
-export const EmptyRating = () => <Select onChange = {action('Value changed')}/>
+export const WithValue = () => {
+    const [value, setValue] = useState('2')
+
+
+   return <>
+        <Select onChange = {setValue}
+                value={value}
+                items={[
+                    {value: '1', title: 'Minsk'},
+                    {value: '2', title: 'Moscow'},
+                    {value: '3', title: 'Kiev'},
+                ]}/>
+    </>
+}
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null)
+
+  return  <>
+        <Select onChange = {setValue}
+                value={value}
+                items={[
+                    {value: '1', title: 'Minsk'},
+                    {value: '2', title: 'Moscow'},
+                    {value: '3', title: 'Kiev'},
+                ]}/>
+    </>
+}
