0d364364aefc12807aca61c08664a8a6ba78ba35
ikomaz97 <ikomaz97@gmail.com>
2024-09-04 18:03:07 +0400

    9634

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 export default {
     title: 'useMemo'
 }
-export const Example1 = () => {
+const NewMassagesCounter = (props: {count:number}) => {
+    return <div>{props.count}</div>
+}
 
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('USER-SECRET')
+    return <div>
+        {
+            props.users.map((u, i) => <div key={i}> {u}) </div>)
+        }
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+    console.log('Example1');
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya'])
 
     return <>
-
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <Users users={users}/>
     </>
 }
