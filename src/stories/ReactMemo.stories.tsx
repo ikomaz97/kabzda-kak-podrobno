5ab00ba93da3f85856001af79dfa5aa7d8f1a04c
ikomaz97 <ikomaz97@gmail.com>
2024-09-04 11:30:52 +0400

    99922eerr66

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -4,7 +4,7 @@ const UsersSecret = (props: {users: Array<string>}) => {
     console.log('USER')
     return <div>
         {
-
+props.users.map((u, i) => <div key={i}> {u}) </div>)
         }
     </div>
 }
