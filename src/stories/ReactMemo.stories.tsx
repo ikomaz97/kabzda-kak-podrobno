260b50a1855c2f7b65c9eb09e03b9d69d1f0356c
ikomaz97 <ikomaz97@gmail.com>
2024-09-04 11:57:05 +0400

    333r

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 
 
 const UsersSecret = (props: {users: Array<string>}) => {
@@ -7,4 +8,17 @@ const UsersSecret = (props: {users: Array<string>}) => {
 props.users.map((u, i) => <div key={i}> {u}) </div>)
         }
     </div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    console.log('Example1');
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya'])
+    const addUsers = () => {
+        const  newUsers = [...users, 'Sveta' + new Date().getTime()];
+        setUsers(newUsers);
+    }
+
 }
