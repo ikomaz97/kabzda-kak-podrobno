942e7c45830713f66f78220c7b141df08b21c9df
ikomaz97 <ikomaz97@gmail.com>
2024-09-04 12:11:07 +0400

    333r777

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,11 +1,15 @@
+
+
 import React, {useState} from "react";
+const NewMassagesCounter = (props: {count:number}) => {
+    return <div>{props.count}</div>
+}
 
-
-const UsersSecret = (props: {users: Array<string>}) => {
+const UsersSecret = (props: { users: Array<string> }) => {
     console.log('USER')
     return <div>
         {
-props.users.map((u, i) => <div key={i}> {u}) </div>)
+            props.users.map((u, i) => <div key={i}> {u}) </div>)
         }
     </div>
 }
@@ -16,9 +20,14 @@ export const Example1 = () => {
     console.log('Example1');
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya'])
-    const addUsers = () => {
-        const  newUsers = [...users, 'Sveta' + new Date().getTime()];
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta' + new Date().getTime()];
         setUsers(newUsers);
     }
-
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}> add user</button>
+        <NewMassagesCounter count = {counter}/>
+        <Users users={users}/>
+</>
 }
