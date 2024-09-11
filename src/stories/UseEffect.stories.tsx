436c7fc231651ee389b0af0c6290155575d1ffc9
ikomaz97 <ikomaz97@gmail.com>
2024-09-11 16:43:10 +0400

    8-924

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,6 +1,24 @@
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 
 export default {
     title: 'useEffect demo'
+}
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1);
+    console.log('SetTimeoutExample');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(state => state + 1);
+        }, 1000);
+
+        return () => clearInterval(interval); // Очистка интервала при размонтировании компонента
+    }, []);
+
+    return (
+        <>
+            Hello, counter: {counter}
+        </>
+    );
 }
