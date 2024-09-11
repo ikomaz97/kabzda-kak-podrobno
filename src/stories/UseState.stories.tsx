9d019f464c73f48d6cb3f1a21f32a25614659ad3
ikomaz97 <ikomaz97@gmail.com>
2024-09-11 16:20:27 +0400

    8-9

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-export default  {
-    title: 'useState demo'
-}
+export default {
+    title: 'useState demo',
+};
 
 export const Example1 = () => {
-console.log('Example1');
-const [counter, setCounter] = useState(0)
-    return <>
-    <button onClick={() => setCounter(counter + 1)}> + </button>
+    console.log('Example1');
+    const [counter, setCounter] = useState(0);
 
-    </>
-}
+    return (
+        <>
+            <p>Counter: {counter}</p>
+            <button onClick={() => setCounter(counter + 1)}> + </button>
+        </>
+    );
+};
